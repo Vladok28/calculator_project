f555325ff00a0a3eef2f24b28ea48fb58aa3a9e6
Vladok28 <50043701+Vladok28@users.noreply.github.com>
2019-12-17 22:04:58 +0500

    kalkulate

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public string d;
+        public string N;
+        public bool N2;
+        public Form1()
+        {
+            N2 = false;
+            InitializeComponent();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if(N2)
+            {
+                N2 = false;
+                textBox1.Text ="0";
+            }
+            Button b = (Button)sender;
+            if(textBox1.Text =="0")
+                textBox1.Text = b.Text;
+            else
+                textBox1.Text = textBox1.Text + b.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// Умножение,деление,сложение, вычитание
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            d = b.Text;
+            N = textBox1.Text; // набранное число в textbox
+
+            N2 = true;
+        }
+
+        //Кнопка равно
+        private void button24_Click(object sender, EventArgs e)
+        {
+            //преобразовываем переменную N в double
+            double DN1, DN2, result;
+            result = 0;
+            DN1 = Convert.ToDouble(N); 
+            DN2 = Convert.ToDouble(textBox1.Text);
+            if (d == "+")
+            {
+                result = DN1 + DN2;
+            }
+            if (d == "-")
+            {
+                result = DN1 - DN2;
+            }
+            if (d == "X")
+            {
+                result = DN1 * DN2;
+            }
+            if (d == "/")
+            {
+                result = DN1 / DN2;
+            }
+           
+
+            d = "=";
+            N2 = true;
+            textBox1.Text = result.ToString();
+
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+        
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void button21_Click(object sender, EventArgs e)
+        {
+           
+        }
+        // точка
+        private void button23_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Contains(",")) 
+            textBox1.Text = textBox1.Text + ",";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Substring(0,textBox1.Text.Length - 1);
+            if (textBox1.Text == "")
+                textBox1.Text = "0";
+        }
+        //проверка ввода на символы
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label1.Text += "\n Откуда: " + Global.where;
+            label1.Text += "\n Куда: " + Global.from;
+            label1.Text += "\n Желаемое время прибытия: " + Global.select1;
+            label1.Text += "\n Особенности заказа: " + Global.select2;
+            Global.select2="";
+            if (Global.phonenumber!="Введите номер телефона")
+            {
+                label1.Text += "\n Номер телефона: " + Global.phonenumber;
+            }
+            else
+            {
+                label1.Text += "\n Номер телефона не указан";
+            }
+            if (Global.comment != "Комментарии к заказу")
+            {
+                label1.Text += "\n Комментарии к заказу: " + Global.comment;
+            }
+            else
+            {
+                label1.Text += "\n Комментарии к заказу отсутствуют";
+            }
+            label1.Text += "\n Выбранный тариф: " + Global.selectcar;
+
+            //Рандом для выбора машинок
+           
+            string[] randoms = new string[6];
+            randoms[0] = "Audi TT Белая М111OO22";
+            randoms[1] = "Chevrolet Corvette Синий О199УЕ74";
+            randoms[2] = "Hyundai Solaris Белый А000ВВ74";
+            randoms[3] = "Лада Vesta SW Серебристая В800ВВ74";
+            randoms[4] = "Toyota Corolla Fielder Белая О901ВЕ38";
+            randoms[5] = "Лада Vesta SW Черная Н840ТС50";
+
+            Random r = new Random();
+            string randsror = randoms[r.Next(0, randoms.Length - 1)];
+            
+            label1.Text += "\n Автомобиль ожидает: "+randoms[new Random().Next(0, randoms.Length - 1)];
+
+        }
+        //возврат на первую форму на Главную
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //переход на третью форму - кнопка Поехали
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 f = new Form3();
+            f.Show();
+
+            
+        }
+    }
+}
